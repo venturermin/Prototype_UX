510d59e43e256a35a3446f4ead9ee4fb31a3f80d
JAEIN1116 <leejaein1116@gmail.com>
2017-12-05 09:39:56 +1000

    dd

--- app/src/main/java/com/bumslap/bum/gitpto/MainActivity.java
+++ app/src/main/java/com/bumslap/bum/gitpto/MainActivity.java
@@ -2,12 +2,26 @@ package com.bumslap.bum.gitpto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.jaein);
+        textView = findViewById(R.id.textViewjaein);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("jaein");
+            }
+        });
     }
 }
