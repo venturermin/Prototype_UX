5217955b60fa49c105f80a30756f3967ba57ca30
bumslap <bumsgoh@gmail.com>
2017-12-05 09:39:18 +0900

    bumswinner

--- app/src/main/java/com/bumslap/bum/gitpto/MainActivity.java
+++ app/src/main/java/com/bumslap/bum/gitpto/MainActivity.java
@@ -2,12 +2,24 @@ package com.bumslap.bum.gitpto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    TextView bumText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bumText = findViewById(R.id.bumText);
+
+
+    }
+
+
+    public void onClickedBum(View v){
+        bumText.setText("gohsangbum");
+
     }
 }
