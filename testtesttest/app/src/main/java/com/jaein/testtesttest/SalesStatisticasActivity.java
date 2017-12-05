6b745dcc87a26e6629b4e34f17acf8b50761cb6a
JAEIN1116 <leejaein1116@gmail.com>
2017-12-05 14:37:58 +1000

    aa

---
+++ testtesttest/app/src/main/java/com/jaein/testtesttest/SalesStatisticasActivity.java
@@ -0,0 +1,79 @@
+package com.jaein.testtesttest;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
+public class SalesStatisticasActivity extends AppCompatActivity {
+        BarChart chart ;
+        ArrayList<BarEntry> BARENTRY ;
+        ArrayList<String> BarEntryLabels ;
+        BarDataSet Bardataset ;
+        BarData BARDATA ;
+        XAxis xAxis;
+        int[] color = {Color.rgb(117,224,233)};
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_sales_statistics);
+            chart = (BarChart) findViewById(R.id.chart1);
+
+            BARENTRY = new ArrayList<>();
+            BarEntryLabels = new ArrayList<String>();
+            AddValuesToBARENTRY();
+            AddValuesToBarEntryLabels();
+            Bardataset  = new BarDataSet(BARENTRY, "Sales");
+            Bardataset.setColors(color);
+
+            Bardataset.setBarSpacePercent(30f);
+
+            BARDATA = new BarData(BarEntryLabels, Bardataset);
+            chart.setData(BARDATA);
+
+            chart.animateY(3000);
+           // xAxis.setTextSize(23.0f);
+            xAxis = chart.getXAxis();
+            xAxis.setTextSize(1f);
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            //xAxis.setLabelRotationAngle();
+        }
+
+        public void AddValuesToBARENTRY(){
+            BARENTRY.add(new BarEntry(2f, 0));
+            BARENTRY.add(new BarEntry(4f, 1));
+            BARENTRY.add(new BarEntry(6f, 2));
+            BARENTRY.add(new BarEntry(8f, 3));
+            BARENTRY.add(new BarEntry(7f, 4));
+            BARENTRY.add(new BarEntry(3f, 5));
+            BARENTRY.add(new BarEntry(2f, 6));
+            BARENTRY.add(new BarEntry(4f, 7));
+            BARENTRY.add(new BarEntry(6f, 8));
+            BARENTRY.add(new BarEntry(8f, 9));
+            BARENTRY.add(new BarEntry(7f, 10));
+            BARENTRY.add(new BarEntry(3f, 11));
+        }
+
+        public void AddValuesToBarEntryLabels(){
+            BarEntryLabels.add("0-2");
+            BarEntryLabels.add("2-4");
+            BarEntryLabels.add("4-6");
+            BarEntryLabels.add("6-8");
+            BarEntryLabels.add("8-10");
+            BarEntryLabels.add("10-12");
+            BarEntryLabels.add("12-14");
+            BarEntryLabels.add("14-16");
+            BarEntryLabels.add("16-18");
+            BarEntryLabels.add("18-20");
+            BarEntryLabels.add("20-22");
+            BarEntryLabels.add("22-24");
+        }
+    }
